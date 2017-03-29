9464217f8c62c6e306354064ee0a706220a33d8e
Gregor von Laszewski <laszewski@gmail.com>
2017-03-29 11:16:00 -0400

    new:usr: add json schema for evegenie

---
+++ cloudmesh/inventory/schema/inventory.settings.py
@@ -0,0 +1,61 @@
+
+host = {
+    'schema': {
+        'name': {
+            'type': 'string'
+        },
+        'cluster': {
+            'type': 'string'
+        },
+        'label': {
+            'type': 'string'
+        },
+        'service': {
+            'type': 'string'
+        },
+        'os': {
+            'type': 'string'
+        },
+        'ip': {
+            'type': 'dict',
+            'schema': {
+                'public': {
+                    'type': 'string'
+                },
+                'private': {
+                    'type': 'string'
+                }
+            }
+        },
+        'project': {
+            'type': 'string'
+        },
+        'owners': {
+            'type': 'list',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'comment': {
+            'type': 'string'
+        },
+        'description': {
+            'type': 'string'
+        },
+        'metadata': {
+            'type': 'string'
+        }
+    }
+}
+
+
+
+eve_settings = {
+    'MONGO_HOST': 'localhost',
+    'MONGO_DBNAME': 'testing',
+    'RESOURCE_METHODS': ['GET', 'POST', 'DELETE'],
+    'BANDWIDTH_SAVER': False,
+    'DOMAIN': {
+        'host': host,
+    },
+}
